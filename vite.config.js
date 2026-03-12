69b48a41d992afc4000da6b0aaf5f856d7349b44
selva selva <Selva-Bharath>
2026-03-11 21:10:23 -0700

    update app logo image

--- vite.config.js
+++ vite.config.js
@@ -15,12 +15,12 @@ export default defineConfig({
         theme_color: "#ffffff",
         icons: [
           {
-            src: "/src/public/logo.png",
+            src: "public/logo.png",
             sizes: "192x192",
             type: "image/png"
           },
           {
-            src: "/src/public/logo.png",
+            src: "public/logo.png",
             sizes: "512x512",
             type: "image/png"
           }
